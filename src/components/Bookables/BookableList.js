91ae103d445e0b92199090194dd3bf38c62346be
Santosh Narayanan <santosh33@gmail.com>
2023-03-04 20:43:27 +0100

    Tracking change with useState

--- src/components/Bookables/BookableList.js
+++ src/components/Bookables/BookableList.js
@@ -1,31 +1,45 @@
 import { useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
 
-import data from "../../static.json"; 
-const {bookables} = data; //Assign array of data to variable - bookables
+import data from "../../static.json";
+const { bookables } = data; //Assign array of data to variable - bookables
+
 
 export default function BookableList() {
    const group = "Rooms";
 
-    //fetch all rooms in bookables list
+   //fetch all rooms in bookables list
    const bookableInGroup = bookables.filter(b => b.group === group);
 
    //Set the index of the first bookable in the group
    const [bookableIndex, setBookableIndex] = useState(0);
    console.log("Room No - " + bookableIndex);
 
-  //render and display name of the rooms from "title key (refer json file" 
+   function nextBookable() {
+      setBookableIndex(i => (i + 1) % bookableInGroup.length);
+   }
+
+   //render and display name of the rooms from "title key (refer json file" 
 
    return (
-      <ul className="bookables items-list-nav">
-         {bookableInGroup.map((b, i) => (
-               <li   key={b.id} 
-                     className={i === bookableIndex ? "selected" : null}
+      <div>
+         <ul className="bookables items-list-nav">
+            {bookableInGroup.map((b, i) => (
+               <li key={b.id}
+                  className={i === bookableIndex ? "selected" : null}
                >
-                  <button className="btn" onClick={()=> setBookableIndex(i)} >
+                  <button className="btn" onClick={() => setBookableIndex(i)} >
                      {b.title}
                   </button>
                </li>
             ))}
-      </ul>
+         </ul>
+         <p>
+            <button className="btn" onClick={nextBookable} autoFocus>
+               <FaArrowRight />
+               <span>Next</span>
+            </button>
+         </p>
+      </div>
    );
 }
