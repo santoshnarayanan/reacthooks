0c6ee57f05651301c8e1e60dbe46ed1b5cdb50b9
Santosh Narayanan <santosh33@gmail.com>
2023-03-06 02:20:14 +0100

    UsersList implemented

--- src/components/Users/UserPicker.js
+++ src/components/Users/UserPicker.js
@@ -1,9 +1,24 @@
-function UserPicker() {
+import { useState, useEffect } from "react";
+import Spinner from "../UI/Spinner";
+
+export default function UserPicker() {
+    const [users, setUsers] = useState(null);
+
+    useEffect(() => {
+        fetch("http://localhost:3001/users")
+          .then((res) => res.json())
+          .then((data) => setUsers(data));
+    }, []);
+
+    if (users === null) {
+        return <Spinner />; 
+    }
+
     return (
         <select>
-            <option>Users</option>
+            {users.map((u) => (
+                <option key={u.id} value={u.id}>{u.name}</option>
+            ))}
         </select>
     );
 }
-
-export default UserPicker;
