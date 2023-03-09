f2197bd758595530d7481c89491aa6d4b74cc969
Santosh Narayanan <santosh33@gmail.com>
2023-03-09 20:07:34 +0100

    Split UserList component

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,20 +1,18 @@
-import { useState, useEffect, Fragment } from 'react';
+import { useState, useEffect } from 'react';
 import Spinner from "../UI/Spinner";
 import getData from "../../utils/api"; // we'll use this api function
 
-export default function UsersList () {
+export default function UsersList({ user, setUser }) {
   // include state for an error object and an isLoading flag
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-
   const [users, setUsers] = useState(null);
-  const [userIndex, setUserIndex] = useState(0);
-  const user = users?.[userIndex];
 
   // update the effect to use the getData function
   useEffect(() => {
     getData("http://localhost:3001/users")
       .then(data => {
+        setUser(data[0]); // set the user to the first user
         setUsers(data);
         setIsLoading(false); // the data has finished loading
       })
@@ -31,39 +29,25 @@ export default function UsersList () {
 
   // alternative UI while users load
   if (isLoading) {
-    return <p><Spinner/> Loading users...</p>
+    return <p><Spinner /> Loading users...</p>
   }
 
   // this UI is unchanged
   return (
-    <Fragment>
-      <ul className="users items-list-nav">
-        {users.map((u, i) => (
-          <li
-            key={u.id}
-            className={i === userIndex ? "selected" : null}
+    <ul className="users items-list-nav">
+      {users.map((u, i) => (
+        <li
+          key={u.id}
+          className={u.id === user?.id ? "selected" : null}
+        >
+          <button
+            className="btn"
+            onClick={() => setUser(u)}
           >
-            <button
-              className="btn"
-              onClick={() => setUserIndex(i)}
-            >
-              {u.name}
-            </button>
-          </li>
-        ))}
-      </ul>
-
-      {user && (
-        <div className="item user">
-          <div className="item-header">
-            <h2>{user.name}</h2>
-          </div>
-          <div className="user-details">
-            <h3>{user.title}</h3>
-            <p>{user.notes}</p>
-          </div>
-        </div>
-      )}
-    </Fragment>
+            {u.name}
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 }
--- src/components/Users/UsersPage.js
+++ src/components/Users/UsersPage.js
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import UsersList from "./UsersList";
+import UserDetails from "./UserDetails";
 
 export default function UsersPage() {
+  const [user, setUser] = useState(null);
+
+  //pass the user state down
   return (
     <main className="users-page">
-      <UsersList />
+      <UsersList user={user} setUser={setUser} />
+      <UserDetails user={user} />
     </main>
   );
 }
