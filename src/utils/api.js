d90d32984d726c8d287c8b3d5f9b48b360fc25c2
Santosh Narayanan <santosh33@gmail.com>
2023-03-15 01:19:38 +0100

    Providing a getBookings data-loading function

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
-export default function getData (url) {
+import { shortISO } from "./date-wrangler";
+export default function getData(url) {
 
   return fetch(url)
     .then(resp => {
@@ -9,4 +10,21 @@ export default function getData (url) {
 
       return resp.json();
     });
+}
+
+
+//Providing a getBookings data-loading function
+export function getBookings(bookableId, startDate, endDate) {
+
+  const start = shortISO(startDate);
+  const end = shortISO(endDate);
+
+  const urlRoot = "http://localhost:3001/bookings";
+
+  //Build up query string
+  const query = `bookableId=${bookableId}` +
+    `&date_gte=${start}&date_lte=${end}`;
+
+  //fetch the bookings returning as promise
+  return getData(`${urlRoot}?${query}`);
 }
