8cb0ce960bc8472a69ba9195d6fa32c165186e52
Santosh Narayanan <santosh33@gmail.com>
2023-03-25 02:39:47 +0100

    Set selected user

--- src/components/Users/UserPicker.js
+++ src/components/Users/UserPicker.js
@@ -1,25 +1,37 @@
 import {useEffect, useState} from "react";
 import Spinner from "../UI/Spinner";
 
-export default function UserPicker() {
-    const [users, setUsers] = useState(null);
+export default function UserPicker ({user, setUser}) {
+  const [users, setUsers] = useState(null);
 
   useEffect(() => {
-
     fetch("http://localhost:3001/users")
       .then(resp => resp.json())
-      .then(data => setUsers(data));
+      .then(data => {
+        setUsers(data);
+        setUser(data[0]);
+      });
+  }, [setUser]);
 
-  }, []);
+  function handleSelect (e) {
+    const selectedID = parseInt(e.target.value, 10);
+    const selectedUser = users.find(u => u.id === selectedID);
 
-    if (users === null) {
-        return <Spinner />
-    }
+    setUser(selectedUser);
+  }
+
+  if (users === null) {
+    return <Spinner/>
+  }
 
   return (
-    <select>
+    <select
+      className="user-picker"
+      onChange={handleSelect}
+      value={user?.id}
+    >
       {users.map(u => (
-        <option key={u.id}>{u.name}</option>
+        <option key={u.id} value={u.id}>{u.name}</option>
       ))}
     </select>
   );
