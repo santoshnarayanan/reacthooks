bd5ce17cc43974b7a937941275958d0ace005d96
Santosh Narayanan <santosh33@gmail.com>
2023-03-25 02:39:58 +0100

    Display edit button

--- src/components/Bookings/BookingDetails.js
+++ src/components/Bookings/BookingDetails.js
@@ -1,9 +1,28 @@
+import {useContext} from "react";
+import {FaEdit} from "react-icons/fa";
 import Booking from "./Booking";
+import UserContext from "../Users/UserContext";
 
 export default function BookingDetails ({booking, bookable}) {
+
+  const user = useContext(UserContext);
+
+  const isBooker = booking && user && (booking.bookerId === user.id);
+
   return (
     <div className="booking-details">
-      <h2>Booking Details</h2>
+      <h2>
+        Booking Details
+        {isBooker && (
+          <span className="controls">
+            <button
+              className="btn"
+            >
+              <FaEdit/>
+            </button>
+          </span>
+        )}
+      </h2>
 
       {booking ? (
         <Booking
